c561102eda0ce0ff0b89eabcd54bfc3f0e39c8bb
Patz sl <gm.teixeira@icloud.com>
2024-05-18 18:51:25 -0300

    feat(layout): integrates header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LayoutModule } from 'modules/feature/layout';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, LayoutModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
