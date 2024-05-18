43ec9e34c4e04ea13145c96a6aaf47be3ec0dbf9
Patz sl <gm.teixeira@icloud.com>
2024-05-18 17:21:52 -0300

    build: add infragistics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,46 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import {
+  IgxButtonModule,
+  IgxCardModule,
+  IgxExpansionPanelModule,
+  IgxForOfModule,
+  IgxIconModule,
+  IgxInputGroupModule,
+  IgxLayoutModule,
+  IgxListModule,
+  IgxNavbarModule,
+  IgxPaginatorModule,
+  IgxRippleModule,
+  IgxSelectModule,
+} from 'igniteui-angular';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(
+      BrowserModule,
+      IgxNavbarModule,
+      IgxIconModule,
+      IgxButtonModule,
+      IgxCardModule,
+      ReactiveFormsModule,
+      IgxSelectModule,
+      IgxInputGroupModule,
+      IgxRippleModule,
+      IgxForOfModule,
+      IgxListModule,
+      IgxPaginatorModule,
+      IgxExpansionPanelModule,
+      IgxLayoutModule
+    ),
+    provideAnimations(),
+  ],
 };
