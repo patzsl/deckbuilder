f8677727cc212accfa7b1b7f87e7156645bb4078
Patz sl <gm.teixeira@icloud.com>
2024-05-19 15:31:40 -0300

    refactor: styling header component with ignite ui and create composition structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule, NoopAnimationsModule],
     }).compileComponents();
   });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { IgxButtonModule, IgxRippleModule } from 'igniteui-angular';
 import { LayoutModule } from 'modules/feature/layout';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LayoutModule],
+  imports: [RouterModule, LayoutModule, IgxButtonModule, IgxRippleModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
