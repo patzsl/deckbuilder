6f632c904c8a058e363463a7aa313c7763a270cc
P̴̻̅̉a̷͙͈̜͋̀t̷̟̓͠z̴̝̣̰͋͑ ̶̲̋̃̔ś̷͓̥̰L̸̖̗̼͑̒̀ <147352311+patzsl@users.noreply.github.com>
2024-05-21 00:16:33 -0300

    feat(login): add login form layout and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'ibm-plex': ['IBM Plex Mono', 'sans-serif'],
+    },
   },
   plugins: [],
 };
